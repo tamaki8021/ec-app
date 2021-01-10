d108f6f31bcc7de3fc796eede1d2eaf1c9e7e0d2
tamaki <hbk2002320@gmail.com>
2021-01-10 18:43:08 +0900

    feat: signin-signup conect-firebase

--- src/reducks/users/operation.js
+++ src/reducks/users/operation.js
@@ -1,26 +1,86 @@
 import { signInAction } from "./actions";
 import { push } from "connected-react-router";
+import { auth, db, FirebaseTimestamp } from "../../firebase/index";
 
-export const signIn = () => {
-  return async (dispatch, getState) => {
-    const state = getState(); //今現在のstateを受け取る
-    const isSignedIn = state.users.isSignedIn;
-
-    if (!isSignedIn) {
-      const url = "https://api.github.com/users/defunkt";
-      const response = await fetch(url)
-        .then((res) => res.json())
-        .catch(() => null);
-      const username = response.login;
-
-      dispatch(
-        signInAction({
-          isSignedIn: true,
-          uid: "00001",
-          username: username,
-        })
-      );
-      dispatch(push('/'));
+export const signIn = (email, password) => {
+  return async (dispatch) => {
+    //Validation
+    if (email === "" || password === "") {
+      alert("必須項目が未入力です");
+      return false; //サインアップ関数はがこれ以上進まないように
     }
+
+    auth.signInWithEmailAndPassword(email, password).then((result) => {
+      const user = result.user;
+
+      if (user) {
+        const uid = user.uid;
+
+        db.collection("users")
+          .doc(uid)
+          .get()
+          .then((snapshot) => {
+            const data = snapshot.data(); //データベースから取得したユーザーの情報
+
+            dispatch(
+              signInAction({
+                isSignedIn: true, //サインインされたよ
+                role: data.role,
+                uid: uid,
+                username: data.username,
+              })
+            );
+
+            dispatch(push("/"));
+          });
+      }
+    });
+  };
+};
+
+export const signUp = (username, email, password, confirmPassword) => {
+  return async (dispatch) => {
+    //Validation
+    if (
+      username === "" ||
+      email === "" ||
+      password === "" ||
+      confirmPassword === ""
+    ) {
+      alert("必須項目が未入力です");
+      return false; //サインアップ関数はがこれ以上進まないように
+    }
+
+    if (password !== confirmPassword) {
+      alert("パスワードが一致しません。もうⅠ度お試しください");
+      return false;
+    }
+
+    return auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        const user = result.user;
+
+        if (user) {
+          const uid = user.uid;
+          const timestamp = FirebaseTimestamp.now();
+
+          const userInitialData = {
+            created_at: timestamp, //このアカウントが作成されたタイム
+            email: email,
+            role: "customer",
+            uid: uid,
+            updated_at: timestamp, //更新された時間
+            username: username,
+          };
+
+          db.collection("users")
+            .doc(uid)
+            .set(userInitialData)
+            .then(() => {
+              dispatch(push("/"));
+            });
+        }
+      });
   };
 };

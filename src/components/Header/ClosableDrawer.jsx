7654314d5ebb22c8b225bd4c3cb4c42e5cd37086
tamaki <hbk2002320@gmail.com>
2021-01-28 12:29:57 +0900

    feat/drawermenu-show-gender-category

--- src/components/Header/ClosableDrawer.jsx
+++ src/components/Header/ClosableDrawer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
@@ -13,9 +13,10 @@ import HistoryIcon from "@material-ui/icons/History";
 import PersonIcon from "@material-ui/icons/Person";
 import ExitToApp from "@material-ui/icons/ExitToApp";
 import { TextInput } from "../UIkit/index";
-import {useDispatch} from 'react-redux'
-import {push} from 'connected-react-router'
-import {signOut} from '../../reducks/users/operation'
+import { useDispatch } from "react-redux";
+import { push } from "connected-react-router";
+import { signOut } from "../../reducks/users/operation";
+import { db } from "../../firebase/index";
 
 const useStyles = makeStyles((theme) => ({
   drawer: {
@@ -38,7 +39,7 @@ const useStyles = makeStyles((theme) => ({
 const ClosableDrawer = (props) => {
   const classes = useStyles();
   const { container } = props;
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const [keyword, setKeyword] = useState("");
 
@@ -50,15 +51,63 @@ const ClosableDrawer = (props) => {
   );
 
   const selectMenu = (event, path) => {
-      dispatch(push(path))
-      props.onClose(event, false)
-  }
+    dispatch(push(path));
+    props.onClose(event, false);
+  };
+
+  const [filters, setFilters] = useState([
+    { func: selectMenu, label: "すべて", id: "all", value: "/" },
+    { func: selectMenu, label: "メンズ", id: "male", value: "/?gender=male" },
+    {
+      func: selectMenu,
+      label: "レディース",
+      id: "female",
+      value: "/?gender=female",
+    },
+  ]);
 
   const menus = [
-    {func: selectMenu, label: '商品登録', icon: <AddCircleIcon />, id: 'register', value: '/product/edit'},
-    {func: selectMenu, label: '注文履歴', icon: <HistoryIcon />, id: 'history', value: '/order/history'},
-    {func: selectMenu, label: 'プロフィール', icon: <PersonIcon />, id: 'profile', value: '/user/mypage'}
-  ]
+    {
+      func: selectMenu,
+      label: "商品登録",
+      icon: <AddCircleIcon />,
+      id: "register",
+      value: "/product/edit",
+    },
+    {
+      func: selectMenu,
+      label: "注文履歴",
+      icon: <HistoryIcon />,
+      id: "history",
+      value: "/order/history",
+    },
+    {
+      func: selectMenu,
+      label: "プロフィール",
+      icon: <PersonIcon />,
+      id: "profile",
+      value: "/user/mypage",
+    },
+  ];
+
+  useEffect(() => {
+    db.collection("categories")
+      .orderBy("order", "asc")
+      .get()
+      .then((snapshots) => {
+        const list = [];
+        snapshots.forEach((snapshot) => {
+          const category = snapshot.data();
+          list.push({
+            func: selectMenu,
+            label: category.name,
+            id: category.id,
+            value: `/?category=${category.id}`,
+          });
+        });
+        setFilters((prevState) => [...prevState, ...list]);
+      });
+  }, []);
 
   return (
     <nav className={classes.drawer}>
@@ -71,7 +120,10 @@ const ClosableDrawer = (props) => {
         classes={{ paper: classes.drawerPeaper }}
         ModalProps={{ keepMounted: true }}
       >
-        <div onClose={(e) => props.onClose(e, false)} onKeyDown={(e) => props.onClose(e, false)} >
+        <div
+          onClose={(e) => props.onClose(e, false)}
+          onKeyDown={(e) => props.onClose(e, false)}
+        >
           <div className={classes.searchField}>
             <TextInput
               fullWidth={false}
@@ -88,12 +140,14 @@ const ClosableDrawer = (props) => {
           </div>
           <Divider />
           <List>
-            {menus.map(menu => (
-              <ListItem button key={menu.id} onClick={(e) => menu.func(e, menu.value)}>
-                  <ListItemIcon>
-                    {menu.icon}
-                  </ListItemIcon>
-                  <ListItemText primary={menu.label} />
+            {menus.map((menu) => (
+              <ListItem
+                button
+                key={menu.id}
+                onClick={(e) => menu.func(e, menu.value)}
+              >
+                <ListItemIcon>{menu.icon}</ListItemIcon>
+                <ListItemText primary={menu.label} />
               </ListItem>
             ))}
             <ListItem button key="logout" onClick={() => dispatch(signOut())}>
@@ -102,6 +156,14 @@ const ClosableDrawer = (props) => {
               </ListItemIcon>
               <ListItemText primary={"ログアウト"} />
             </ListItem>
+          </List>
+          <Divider />
+          <List>
+            {filters.map((filter) => (
+              <ListItem button key={filter.id} onClick={(e) => filter.func(e, filter.value)} >
+                <ListItemText primary={filter.label} />
+              </ListItem>
+            ))}
           </List>
         </div>
       </Drawer>
